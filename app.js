91b53ad171c1d079e1965f49b3e31196b4e7ae49
PRIYANSHU DAS <ask.priyanshu@gmail.com>
2021-09-30 23:03:37 +0530

    Adding Code csnippet copy funtionality(Light theme)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const fs = require('fs')
 const PORT = process.env.PORT || 8080
 const {
     generateHTMLPage
@@ -16,7 +16,8 @@ app.post('/', function (req, res) {
     let markDownData = req.body.data;
     let themeData = req.body.theme;
     let htmlData = generateHTMLPage(markDownData, themeData);
-    console.log(req.body.theme)
+    console.log(req.body.theme);
+    fs.writeFileSync('htmlfile.html', htmlData);
     res.send(htmlData);
 })
 
