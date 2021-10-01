69eb411e6b00bb0a6de267390ba5d2a937df54e0
PRIYANSHU DAS <ask.priyanshu@gmail.com>
2021-10-01 18:46:35 +0530

    Added option for getting only div instead of html page

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-const fs = require('fs')
+const fs = require('fs');
+const {
+    type
+} = require('os');
 const PORT = process.env.PORT || 8080
 const {
-    generateHTMLPage
+    generateHTMLPage,
+    generateDiv
 } = require('./pageify')
 
 app.use(express.urlencoded({
@@ -15,10 +19,17 @@ app.use(express.json());
 app.post('/', function (req, res) {
     let markDownData = req.body.data;
     let themeData = req.body.theme;
-    let htmlData = generateHTMLPage(markDownData, themeData);
+    let typeData = req.body.type; // div or page
+    let title = req.body.title;
+    let resData = undefined;
+
+    if (typeData === 'page')
+        resData = generateHTMLPage(markDownData, themeData, title);
+    else if (typeData === 'div')
+        resData = generateDiv(markDownData, themeData)
     console.log(req.body.theme);
-    fs.writeFileSync('htmlfile.html', htmlData);
-    res.send(htmlData);
+    fs.writeFileSync('htmlfile.html', resData);
+    res.send(resData);
 })
 
 
--- pageify.js
+++ pageify.js
@@ -18,12 +18,12 @@ const copyCodeSuccessColors = {
 }
 
 
-function generateHTMLPage(markDownData, themeData) {
+function generateHTMLPage(markDownData, themeData, title) {
 
     let htmlData = xssFilter(converter.makeHtml(markDownData));
     converter.setFlavor('github');
     let styleData;
-    let pageTitle = "Hello";
+    let pageTitle = title;
 
 
     styleData = fs.readFileSync(__dirname + `/style-${themeData}.css`);
@@ -47,12 +47,27 @@ function generateHTMLPage(markDownData, themeData) {
                 <meta name="viewport" content="width=device-width, initial-scale=1">
             </head>
             <body>
+            <h1 class="title">${pageTitle}</h1>
                 <div id='content'>
             `
 
     let postContent = `
                 </div>
-                <style type='text/css'>` + styleData + `</style>
+                <style type='text/css'>` + styleData + `.title {
+                    margin: 0px;
+                    max-width: 900px;
+                    padding: 10px 40px;
+                    padding-bottom: 20px;
+                    margin-left: auto;
+                    margin-right: auto;
+
+                }
+                @media only screen and (max-width: 600px) {
+                    .title{
+                        padding: 0px 20px 20px 20px !important;
+                    }
+                }
+                </style>
                 <script>
                     const codes = document.querySelectorAll("pre")
                     codes.forEach((codeBlock) => {
@@ -102,6 +117,82 @@ function generateHTMLPage(markDownData, themeData) {
 
 }
 
+
+function generateDiv(markDownData, themeData) {
+    let htmlData = xssFilter(converter.makeHtml(markDownData));
+    converter.setFlavor('github');
+    let styleData;
+
+    styleData = fs.readFileSync(__dirname + `/style-${themeData}.css`);
+
+    let copyIcon = `
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 512 512" style="display: block;" transform="scale(0.65)">
+                            <g>
+                                <g>
+                                <path fill="000000" d="M480.6,109.1h-87.5V31.4c0-11.3-9.1-20.4-20.4-20.4H31.4C20.1,11,11,20.1,11,31.4v351c0,11.3,9.1,20.4,20.4,20.4h87.5    v77.7c0,11.3,9.1,20.4,20.4,20.4h341.3c11.3,0,20.4-9.1,20.4-20.4v-351C501,118.3,491.9,109.1,480.6,109.1z M51.8,362V51.8h300.4    v57.3H139.3c-11.3,0-20.4,9.1-20.4,20.4V362H51.8z M460.2,460.2H159.7V150h300.4V460.2z"/>
+                                <path fill="000000" d="m233.3,254.4h155.8c11.3,0 20.4-9.1 20.4-20.4 0-11.3-9.1-20.4-20.4-20.4h-155.8c-11.3,0-20.4,9.1-20.4,20.4 0,11.2 9.1,20.4 20.4,20.4z"/>
+                                <path d="m233.3,396.6h155.8c11.3,0 20.4-9.1 20.4-20.4 0-11.3-9.1-20.4-20.4-20.4h-155.8c-11.3,0-20.4,9.1-20.4,20.4 0,11.3 9.1,20.4 20.4,20.4z"/>
+                                </g>
+                            </g>
+                        </svg>
+                    `
+
+    let preContent = `
+
+                <div id='markdown' class='markdown-div'>
+            `
+
+    let postContent = `
+                </div>
+                <style type='text/css'>` + styleData + `</style>
+                <script>
+                    const codes = document.querySelectorAll("pre")
+                    codes.forEach((codeBlock) => {
+                        let htmlCode = codeBlock.innerHTML
+
+                        htmlCode += \`
+                                        <button class=\"copy-code-button\" onclick=\"copyCode(this)\">
+                                            ${copyIcon}
+                                        </button>
+                                        <span class = \"copy-code-success-message\">
+                                            Copied!
+                                        </span>
+                                    \`
+                        codeBlock.innerHTML = htmlCode
+                    });
+
+                    function copyCode(event) {
+                        const color = "${copyCodeSuccessColors[themeData]}";
+                        const codeBlock = event.parentElement;
+                        let codeSnippet = codeBlock.querySelector('code').innerText;
+                        console.log(codeSnippet);
+                        const cb = navigator.clipboard;
+                        cb.writeText(codeSnippet).then(() => {
+                            const copyMessage = codeBlock.querySelector('.copy-code-success-message');
+                            const codeCopyButton = codeBlock.querySelector('.copy-code-button');
+                            const codeCopyButtonSVG = codeBlock.querySelector('.copy-code-button svg');
+                            copyMessage.style.opacity = 1;
+                            copyMessage.style.color = color;
+                            codeCopyButton.setAttribute('style', \`border-color: \${color}!important \`);
+                            codeCopyButtonSVG.setAttribute('style', \`fill: \${color}!important \`);
+                            console.log(codeCopyButton);
+                            setTimeout(() => {
+                                copyMessage.style.opacity = 0;
+                                codeCopyButton.setAttribute('style', 'border-color:#40454C60');
+                                codeCopyButtonSVG.setAttribute('style', 'fill:#80858C99');
+                            }, 600);
+                        });
+                    }
+                </script>
+            `;
+
+
+
+
+    return preContent + htmlData + postContent
+}
+
 module.exports = {
-    generateHTMLPage
+    generateHTMLPage,
+    generateDiv
 }
