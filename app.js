c8158c174c64b641cb61cf488b63aae20f503f06
ParadoxPD <ask.priyanshu@gmail.com>
2022-08-04 16:59:58 +0530

    Changes

--- app.js
+++ app.js
@@ -1,38 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require('fs');
-const {
-    type
-} = require('os');
-const PORT = process.env.PORT || 8080
-const {
-    generateHTMLPage,
-    generateDiv
-} = require('./pageify')
+const fs = require("fs");
+const PORT = process.env.PORT || 5000;
+const { generateHTMLPage, generateDiv } = require("./pageify");
 
-app.use(express.urlencoded({
-    extended: false
-}));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 app.use(express.json());
 
+app.post("/", function (req, res) {
+  let markDownData = req.body.data;
+  let themeData = req.body.theme;
+  let typeData = req.body.type; // div or page
+  let title = req.body.title;
+  let resData = undefined;
 
-app.post('/', function (req, res) {
-    let markDownData = req.body.data;
-    let themeData = req.body.theme;
-    let typeData = req.body.type; // div or page
-    let title = req.body.title;
-    let resData = undefined;
-
-    if (typeData === 'page')
-        resData = generateHTMLPage(markDownData, themeData, title);
-    else if (typeData === 'div')
-        resData = generateDiv(markDownData, themeData)
-    console.log(req.body.theme);
-    fs.writeFileSync('htmlfile.html', resData);
-    res.send(resData);
-})
-
+  if (typeData === "page")
+    resData = generateHTMLPage(markDownData, themeData, title);
+  else if (typeData === "div") resData = generateDiv(markDownData, themeData);
+  console.log(req.body.theme);
+  // fs.writeFileSync('htmlfile.html', resData);
+  res.send(resData);
+});
 
 const server = app.listen(PORT, function () {
-    console.log(`App listening at port ${PORT}`);
-})
+  console.log(`App listening at port ${server.address().port}`);
+});
